7b3f384428e134f8bb8e22c0e2e679f570f9a022
root <root@designsafe-data-01.tacc.utexas.edu>
2016-10-31 15:40:39 -0500

    Commenting out virtualenv/kernel creation for now.

--- designsafe/entry.sh
+++ designsafe/entry.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 
-if [ ! -d "/home/jupyter/mydata/virtualenvs" ]; then
+#if [ ! -d "/home/jupyter/mydata/virtualenvs" ]; then
 #    cp -r /home/jupyter/virtualenvs /home/jupyter/mydata/
+#
+#    echo "creating virtualenvs directory."
+#    mkdir -p /home/jupyter/mydata/virtualenvs
 
-    echo "creating virtualenvs directory."
-    mkdir -p /home/jupyter/mydata/virtualenvs
-
-    echo "creating py2 virtualenv"
+#    echo "creating py2 virtualenv"
     # create py2 virtualenv
-    cd /home/jupyter/mydata/virtualenvs && virtualenv -p /usr/bin/python2 py2
-    . /home/jupyter/mydata/virtualenvs/py2/bin/activate && pip install ipykernel && deactivate;
+#    cd /home/jupyter/mydata/virtualenvs && virtualenv -p /usr/bin/python2 py2
+#    . /home/jupyter/mydata/virtualenvs/py2/bin/activate && pip install ipykernel && deactivate;
 
     # create py3 virtualenv
-    echo "creating py3 virtualenv"
-    export LD_LIBRARY_PATH=/opt/conda/lib
-    cd /home/jupyter/mydata/virtualenvs && virtualenv -p /opt/conda/bin/python3 py3
-    . /home/jupyter/mydata/virtualenvs/py3/bin/activate && pip install ipykernel && deactivate;
-
-fi
+#    echo "creating py3 virtualenv"
+#    export LD_LIBRARY_PATH=/opt/conda/lib
+#    cd /home/jupyter/mydata/virtualenvs && virtualenv -p /opt/conda/bin/python3 py3
+#    . /home/jupyter/mydata/virtualenvs/py3/bin/activate && pip install ipykernel && deactivate;
+#
+#fi
 
 
 # start jupyter
